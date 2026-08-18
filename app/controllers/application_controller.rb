58f20309c59415060cf891905f710d86bc1b9fbf
Kylie LIN <kylvi@live.com>
2026-08-18 17:29:46 +0900

    application_controller for the current user nickname, landing pagedesign, deleted the replaceholder of the action in the drop down menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_unread_count
+
+  private
+
+  def set_unread_count
+    @unread_count = 3 if user_signed_in?
+  end
+
+  def after_sign_in_path_for(resource)
+    ui_kits_path
+  end
 end
